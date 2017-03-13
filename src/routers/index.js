8c1e1c0c3e639c5c3e9202f7712355babd55e1e0
luoboding <dengpengks@gmail.com>
2017-03-13 16:15:46 +0800

    add amap

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router';
 import configStore from './../stores';
 import LoginContainer from './../containers/login-container';
 import authSaga from './../reducers/auth.saga';
 import HomeContainer from './../containers/home-container';
 import Welcome from './../components/welcome';
-import OrderContainer from './../containers/order-container.js';
+import OrderContainer from './../containers/news-container.js';
+import ScenicContainer from './../containers/scenic-container';
 
 const store = configStore();
 store.runSaga(authSaga);
 
 const Root = () => (
 	<Provider store={store}>
-		<Router history={hashHistory}>
+		<Router history={browserHistory}>
       <Route path='/login' component={LoginContainer} />
 			<Route path="/" component={HomeContainer}>
         <IndexRoute component={Welcome} />
-        <Route path='/order' component={OrderContainer} />
+        <Route path='/news' component={OrderContainer} />
+        <Route path='/scenic' component={ScenicContainer} />
       </Route>
 		</Router>
 	</Provider>
