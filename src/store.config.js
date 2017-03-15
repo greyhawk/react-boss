e689153519be53578bf707789080090f57415468
luoboding <dengpengks@gmail.com>
2017-03-15 15:22:45 +0800

    confict resolve

--- src/store.config.js
+++ src/store.config.js
@@ -1,10 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-<<<<<<< HEAD:src/store.config.js
 import reducers from './reducers';
-import thunkMiddleware from 'redux-thunk';
-=======
-import reducers from './../reducers';
->>>>>>> 6d554e8c44a9dc79de1ed871dfadbb98e309cd38:src/stores/index.js
 import createLogger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga'
 
